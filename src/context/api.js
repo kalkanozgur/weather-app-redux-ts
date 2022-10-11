94e432872a170400d8a7ae32d7b286c7d990cd21
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-10-11 13:12:59 +0300

    api update

--- src/context/api.js
+++ src/context/api.js
@@ -3,17 +3,19 @@ import axios from "axios";
 const weather_url = `https://api.weatherbit.io/v2.0/forecast/daily`;
 const geocoding_url = `https://api.api-ninjas.com/v1/geocoding`; //?city=London&country=England
 
-export const fetchWeatherApi = async (lat, lon) => {
+export const fetchWeatherApi = async ({ latitude, longitude }) => {
+	console.log({ latitude, longitude });
 	const res = await axios(
-		`${weather_url}?days=7&lat=${lat}&lon=${lon}&days=7&key=${process.env.REACT_APP_WEATHER_API_KEY}`
+		`${weather_url}?&days=7&lat=${latitude}&lon=${longitude}&days=7&key=${process.env.REACT_APP_WEATHER_API_KEY}`
 	);
+	console.log(res.data);
 	return res.data;
 };
 
-export const fetchCityApi = async (city) => {
-	console.log({ "X-Api-Key": process.env.REACT_APP_APININJAS_API_KEY });
-	const res = await axios(`${geocoding_url}?city=${city}`, {
+export const fetchCityApi = async (cityName) => {
+	const res = await axios(`${geocoding_url}?city=${cityName}`, {
 		headers: { "X-Api-Key": process.env.REACT_APP_APININJAS_API_KEY },
 	});
+	console.log(res.data);
 	return res.data;
 };
